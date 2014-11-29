66d687bbe34af88c1c466fc6902964db819ae9ef
Elan Ruusamäe <glen@delfi.ee>
2014-11-29 20:54:15 +0200

    cvs: restructure code to be more modular

--- eventum-cvs-hook.php
+++ eventum-cvs-hook.php
@@ -29,6 +29,23 @@
 // |          Elan Ruusamäe <glen@delfi.ee>                               |
 // +----------------------------------------------------------------------+
 
+/**
+ * Setup in your CVS server:
+ *
+ * for CVS 1.11:
+ * CVSROOT/loginfo:
+ * # process any message with eventum
+ * ALL /path/to/eventum-cvs-hook.php $USER %{sVv}
+ *
+ * for CVS 1.12:
+ * CVSROOT/loginfo:
+ * # process any message with eventum
+ * ALL /path/to/eventum-cvs-hook.php $USER "%p" %{sVv}
+ *
+ * CVSROOT/config:
+ * UseNewInfoFmtStrings=yes
+ */
+
 // URL to your Eventum installation.
 // https is supported transparently by PHP 5 if you have openssl module enabled.
 $eventum_url = 'http://eventum.example.com/';
@@ -38,6 +55,10 @@ $scm_name = 'cvs';
 //
 // DO NOT CHANGE ANYTHING AFTER THIS LINE
 //
+
+// save name of this script
+$PROGRAM = basename(array_shift($argv));
+
 $dir = dirname(__FILE__);
 require_once "$dir/helpers.php";
 
@@ -47,70 +68,15 @@ if (file_exists($configfile)) {
     require_once $configfile;
 }
 
-// save name of this script
-$PROGRAM = basename(array_shift($argv));
-// save who is committing these changes
-$username = array_shift($argv);
-
-// grab fileinfo
-if ($argc == 3) {
-    // assume the old way ("PATH {FILE,rev1,rev2 }+")
-    // CVSROOT/loginfo: ALL eventum-cvs-hook $USER %{sVv}
-    $args = explode(' ', array_shift($argv));
-    // save what the name of the module is
-    $cvs_module = array_shift($args);
-
-    // skip if we're importing or adding new dirrectory
-    // TODO: checked old way with CVS 1.11, but not checked the new way
-    $msg = implode(' ', array_slice($args, -3));
-    if (in_array($msg, array('- Imported sources', '- New directory'))) {
-        exit(0);
-    }
-
-    // now parse the list of modified files
-    $modified_files = array();
-    foreach ($args as $file_info) {
-        list($filename, $old_revision, $new_revision) = explode(',', $file_info);
-        $modified_files[] = array(
-            'filename'     => $filename,
-            'old_revision' => $old_revision,
-            'new_revision' => $new_revision
-        );
-    }
-
-} else {
-    // assume the new way ("PATH" {"FILE" "rev1" "rev2"}+)
-    // CVSROOT/loginfo: ALL eventum-cvs-hook $USER "%p" %{sVv}
-    $args = $argv;
-    // save what the name of the module is
-    $cvs_module = array_shift($args);
-
-    // skip if we're importing or adding new dirrectory
-    // TODO: checked old way with CVS 1.11, but not checked the new way
-    $msg = implode(' ', array_slice($args, -3));
-    if (in_array($msg, array('- Imported sources', '- New directory'))) {
-        exit(0);
-    }
-
-    // now parse the list of modified files
-    $modified_files = array();
-    while ($file_info = array_splice($args, 0, 3)) {
-        list($filename, $old_revision, $new_revision) = $file_info;
-        $modified_files[] = array(
-            'filename'     => $filename,
-            'old_revision' => $old_revision,
-            'new_revision' => $new_revision
-        );
-    }
-}
-
-// get the full commit message
-$input = stream_get_contents(STDIN);
-$commit_msg = rtrim(substr($input, strpos($input, 'Log Message:') + strlen('Log Message:') + 1));
+$commit_msg = cvs_commit_msg();
 
 // parse the commit message and get all issue numbers we can find
 $issues = match_issues($commit_msg);
+
 if ($issues) {
+    // grab fileinfo
+    list($username, $cvs_module, $modified_files) = cvs_commit_info($argv);
+
     // need to encode all of the url arguments
     $commit_msg = rawurlencode($commit_msg);
     $cvs_module = rawurlencode($cvs_module);
@@ -131,7 +97,7 @@ if ($issues) {
 
     $res = wget($ping_url, true);
     if (!$res) {
-        fwrite(STDERR, "Error: Couldn't read response from $ping_url\n");
+        error_log("Error: Couldn't read response from $ping_url");
         exit(1);
     }
 
@@ -140,7 +106,7 @@ if ($issues) {
     $status = array_shift($headers);
     list($proto, $status, $msg) = explode(' ', trim($status), 3);
     if ($status != '200') {
-        fwrite(STDERR, "Error: Could not ping the Eventum SCM handler script: HTTP status code: $status $msg\n");
+        error_log("Error: Could not ping the Eventum SCM handler script: HTTP status code: $status $msg");
         exit(1);
     }
 
@@ -148,4 +114,117 @@ if ($issues) {
     foreach (explode("\n", trim($data)) as $line) {
         echo "$PROGRAM: $line\n";
     }
+}
+
+/**
+ * @param array $argv
+ * @return array of username, cvs module, modified files
+ */
+function cvs_commit_info($argv) {
+    // user who is committing these changes
+    $username = array_shift($argv);
+
+    if (count($argv) == 3) {
+        $info = cvs_parse_info_1_11($argv);
+    } else {
+        $info = cvs_parse_info_1_12($argv);
+    }
+
+    // parse told to skip (for example adding new dir)
+    if (!$info) {
+        exit(0);
+    }
+
+    list($cvs_module, $modified_files) = $info;
+
+    return array($username, $cvs_module, $modified_files);
+}
+
+/**
+ * assume the old way ("PATH {FILE,rev1,rev2 }+")
+ * CVSROOT/loginfo: ALL eventum-cvs-hook $USER %{sVv}
+ *
+ * @return array
+ */
+function cvs_parse_info_1_11($argv) {
+    $args = explode(' ', array_shift($argv));
+
+    // save what the name of the module is
+    $cvs_module = array_shift($args);
+
+    // skip if we're importing or adding new dirrectory
+    $msg = implode(' ', array_slice($args, -3));
+    if (in_array($msg, array('- Imported sources', '- New directory'))) {
+        return null;
+    }
+
+    // now parse the list of modified files
+    $modified_files = array();
+    foreach ($args as $file_info) {
+        list($filename, $old_revision, $new_revision) = explode(',', $file_info);
+        $modified_files[] = array(
+            'filename'     => $filename,
+            'old_revision' => cvs_filter_none($old_revision),
+            'new_revision' => cvs_filter_none($new_revision),
+        );
+    }
+
+    return array($cvs_module, $modified_files);
+}
+
+/**
+ * assume the new way ("PATH" {"FILE" "rev1" "rev2"}+)
+ * CVSROOT/loginfo: ALL eventum-cvs-hook $USER "%p" %{sVv}
+ *
+ * @return array
+ */
+function cvs_parse_info_1_12($args) {
+    // save what the name of the module is
+    $cvs_module = array_shift($args);
+
+    // skip if we're importing or adding new directory
+    // TODO: checked old way with CVS 1.11, but not checked the new way
+    $msg = implode(' ', array_slice($args, -3));
+    if (in_array($msg, array('- Imported sources', '- New directory'))) {
+        return null;
+    }
+
+    // now parse the list of modified files
+    $modified_files = array();
+    while ($file_info = array_splice($args, 0, 3)) {
+        list($filename, $old_revision, $new_revision) = $file_info;
+        $modified_files[] = array(
+            'filename'     => $filename,
+            'old_revision' => cvs_filter_none($old_revision),
+            'new_revision' => cvs_filter_none($new_revision),
+        );
+    }
+
+    return array($cvs_module, $modified_files);
+}
+
+/**
+ * filter out NONE revision
+ *
+ * @param string $rev
+ * @return null
+ */
+function cvs_filter_none($rev)
+{
+    if ($rev != 'NONE') {
+        return $rev;
+    }
+    return null;
+}
+
+/**
+ * Obtain CVS commit message
+ * @return string
+ */
+function cvs_commit_msg()
+{
+    // get the full commit message
+    $input = stream_get_contents(STDIN);
+    $commit_msg = rtrim(substr($input, strpos($input, 'Log Message:') + strlen('Log Message:') + 1));
+    return $commit_msg;
 }
