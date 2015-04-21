2a83268a28b4243cb43bbeca9627cd595ee8ebe7
Elan Ruusamäe <glen@delfi.ee>
2015-04-21 13:47:33 +0300

    fix cvs hook handling in old (1.11) cvs server

--- eventum-cvs-hook.php
+++ eventum-cvs-hook.php
@@ -6,7 +6,7 @@
 // +----------------------------------------------------------------------+
 // | Copyright (c) 2003 - 2008 MySQL AB                                   |
 // | Copyright (c) 2008 - 2010 Sun Microsystem Inc.                       |
-// | Copyright (c) 2011 - 2014 Eventum Team.                              |
+// | Copyright (c) 2011 - 2015 Eventum Team.                              |
 // |                                                                      |
 // | This program is free software; you can redistribute it and/or modify |
 // | it under the terms of the GNU General Public License as published by |
@@ -22,7 +22,7 @@
 // | along with this program; if not, write to:                           |
 // |                                                                      |
 // | Free Software Foundation, Inc.                                       |
-// | 51 Franklin Street, Suite 330                                          |
+// | 51 Franklin Street, Suite 330                                        |
 // | Boston, MA 02110-1301, USA.                                          |
 // +----------------------------------------------------------------------+
 // | Authors: João Prado Maia <jpm@mysql.com>                             |
@@ -114,7 +114,7 @@ function cvs_commit_info($argv)
     // user who is committing these changes
     $username = array_shift($argv);
 
-    if (count($argv) == 3) {
+    if (count($argv) == 1) {
         $info = cvs_parse_info_1_11($argv);
     } else {
         $info = cvs_parse_info_1_12($argv);
